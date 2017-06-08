bea535655ae97b53709eba89118086d22c586266
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-06-08 14:57:44 -0400

    try to use set difference and set intersection
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-var myModule = require('./node_modules/kush')("It is a beautiful day");
-console.log(myModule.helloPerson('Sally'));
+var setDifference = require('./node_modules/kush/set-difference');
+var setIntersection = require('./node_modules/kush/set-intersection');
+
+var set1 = ['dogs', 'cats', 'red', 'bananas', 'code', 'movies'];
+var set2 = ['blue', 'horses', 'dogs', 'code', 'rain'];
+
+var difference = setDifference(set1, set2);
+var intersection = setIntersection(set1, set2);
+
+console.log('intersection ' + intersection);
+console.log('difference ' + difference);
